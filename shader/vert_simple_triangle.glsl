d9803acde242345883e54703f72c9eb4469cc31d
Yingbo Li <liveitbe@126.com>
2020-04-29 11:52:10 +0800

    NOTE: uniform is a keyword;

--- shader/vert_simple_triangle.glsl
+++ shader/vert_simple_triangle.glsl
@@ -5,7 +5,7 @@ layout(binding = 0) uniform UniformBuffer {
     mat4 model;
     mat4 view;
     mat4 proj;
-} uniform;
+} ubo;
 
 layout(location = 0) in vec2 position;
 layout(location = 1) in vec3 color;
@@ -13,6 +13,6 @@ layout(location = 1) in vec3 color;
 layout(location = 0) out vec3 fragColor;
 
 void main() {
-    gl_Position = uniform.model * uniform.view * uniform.proj * vec4(position, 0.0, 1.0);
+    gl_Position = ubo.model * ubo.view * ubo.proj * vec4(position, 0.0, 1.0);
     fragColor = color;
 }
