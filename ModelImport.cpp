bda75af79f5c8e894402789d747d8cfa5b012979
Yingbo Li <liveitbe@126.com>
2020-05-12 00:40:11 +0800

    load gltf;

--- ModelImport.cpp
+++ ModelImport.cpp
@@ -2,6 +2,8 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 #define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_STB_IMAGE
+#define TINYGLTF_NO_STB_IMAGE_WRITE
 #include "tiny_gltf.h"
 #include <unordered_map>
 #include <iostream>
@@ -24,11 +26,14 @@ bool ModelImport::loadObj(const char* path, const float scale, MeshInput* mesh) 
 		std::cout << warning << "\n----\n" << error << std::endl;
 		return false;
 	}
+	if (!warning.empty()) {
+		std::cout << warning << std::endl;
+	}
 
 	std::unordered_map<Vertex, uint32_t> vertexIndex;
 	VertexIndexed data;
 	for (const auto& shape : shapeList) {
-		//shape.name;
+		std::cout << shape.name << "\n";
 		for (const auto& indexInfo : shape.mesh.indices) {
 			Vertex vertex{};
 			auto index = indexInfo.vertex_index * 3;
@@ -56,12 +61,44 @@ bool ModelImport::loadObj(const char* path, const float scale, MeshInput* mesh) 
 	}
 	std::cout << data.vertices.size() << "|" << data.indices.size() << std::endl;
 	mesh->setData(std::move(data));
+	//TODO manage material/texture
 	return true;
 }
 
-bool ModelImport::loadGltf(const char* path, const float scale, MeshInput* mesh) const {
-	
-	return false;
+bool ModelImport::loadGltf(const char* path, const float scale, const bool isBinary, MeshInput* mesh) const {
+	tinygltf::Model model;
+	tinygltf::TinyGLTF loader;//TODO should this be reused?
+	std::string warning, error;
+	bool loadResult;
+	if (isBinary) {
+		loadResult = loader.LoadBinaryFromFile(&model, &warning, &error, path);
+	}
+	else {
+		loadResult = loader.LoadASCIIFromFile(&model, &warning, &error, path);
+	}
+	if (loadResult == false) {
+		std::cout << warning << "\n----\n" << error << std::endl;
+		return false;
+	}
+	if (!warning.empty()) {
+		std::cout << warning << std::endl;
+	}
+	std::cout << "scene count = " << model.scenes.size() << " default = " << model.defaultScene << std::endl;
+	auto sceneIndex = model.defaultScene > -1 ? model.defaultScene : 0;
+	tinygltf::Scene scene = model.scenes[sceneIndex];
+	//TODO currently scene structure ignored
+	std::cout << "name = " << scene.name << " node = " << scene.nodes.size() << " |";
+	for (const auto& node : scene.nodes) {
+		std::cout << node << " ";
+	}
+	std::cout << "\n";
+	std::cout << "bufferView = " << model.bufferViews.size() << "\n";
+	for (const auto& view : model.bufferViews) {
+		const auto& buffer = model.buffers[view.buffer];
+		std::cout << buffer.name << "/" << buffer.data.size() << "\n";
+	}
+	std::cout << std::endl;
+	return true;
 }
 
 bool ModelImport::load(const std::string& path, const float scale, MeshInput* mesh) const {
@@ -69,8 +106,11 @@ bool ModelImport::load(const std::string& path, const float scale, MeshInput* me
 	if (stringEndsWith(path, ".obj")) {
 		return loadObj(path.c_str(), scale, mesh);
 	}
-	else if (stringEndsWith(path, ".glft") || stringEndsWith(path, ".glb")) {
-		return loadGltf(path.c_str(), scale, mesh);
+	else if (stringEndsWith(path, ".glft")) {
+		return loadGltf(path.c_str(), scale, false, mesh);
+	}
+	else if (stringEndsWith(path, ".glb")) {
+		return loadGltf(path.c_str(), scale, true, mesh);
 	}
 	std::cout << "unknown format" << std::endl;
 	return result;
--- ModelImport.h
+++ ModelImport.h
@@ -6,7 +6,7 @@ class ModelImport
 {
 private:
 	bool loadObj(const char* path, const float scale, MeshInput* mesh) const;
-	bool loadGltf(const char* path, const float scale, MeshInput* mesh) const;
+	bool loadGltf(const char* path, const float scale, const bool isBinary, MeshInput* mesh) const;
 public:
 	bool load(const std::string& path, const float scale, MeshInput* mesh) const;
 };
